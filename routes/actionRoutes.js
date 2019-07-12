a326ce0859fc831e5dbd7ed2c19e51628bf80fc8
tjr226 <tjr226@gmail.com>
2019-07-12 12:29:32 -0400

    added verify action middleware, POST action route

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const actionDB = require('../data/helpers/actionModel.js');
+const projectDB = require('../data/helpers/projectModel.js');
 
 // POST
+router.post('/', validateAction, (req, res) => {
+    const actionInfo = req.body;
+    actionDB.insert(actionInfo)
+    .then(action => {
+        res.status(201).json(action);
+    })
+    .catch(error => {
+        res.status(500).json({ error: "The action could not be created." });
+    })
+})
 
 // GET ALL
 router.get('/', (req, res) => {
@@ -48,5 +59,30 @@ function validateActionId(req, res, next) {
         return res.status(500).json({ error: "The action ID could not be verified." });
     })
 }
+
+function validateAction(req, res, next) {
+    const actionInfo = req.body;
+    
+    if (actionInfo.project_id === undefined ||
+        actionInfo.description === undefined ||
+        actionInfo.notes === undefined ||
+        actionInfo.description.length > 128) {
+            return res.status(400).json({ errorMessage: "Please include project ID, description under 128 chars, and notes."})
+        } else {
+            projectDB.get(actionInfo.project_id)
+            .then(project => {
+                if (project === null) {
+                    return res.status(404).json({ message: "The project ID does not exist." });
+                } else {
+                    next();
+                }
+            })
+            .catch(error => {
+                return res.status(500).json({ error: "The project ID could not be verified." });
+            })
+        }
+}
+
+
 
 module.exports = router;
